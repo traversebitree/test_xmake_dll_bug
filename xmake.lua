e1326aaa67441748ca0500b43f82a0021eced142
Kong <xykong421@outlook.com>
2024-02-04 22:44:05 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,25 +2,28 @@ add_rules("mode.debug", "mode.release")
 add_rules("set_export_all_symbols")
 
 rule("set_export_all_symbols")
-do 
-    on_load(function (target)
-        if target:kind()=="static" then
-            target:set("kind","static")
-        elseif target:kind()=="shared" then
-            target:set("kind","shared")
-            if is_plat("windows") then 
-                import("core.project.rule")
-                local rule = rule.rule("utils.symbols.export_all")
-                target:rule_add(rule)
-                target:extraconf_set("rules", "utils.symbols.export_all", {export_classes = true})
+do
+    on_load(
+        function(target)
+            if target:kind() == "static" then
+                target:set("kind", "static")
+            elseif target:kind() == "shared" then
+                import("core.tool.toolchain")
+                target:set("kind", "shared")
+                if is_plat("windows") and toolchain.load("msvc"):config("vs") then
+                    import("core.project.rule")
+                    local rule = rule.rule("utils.symbols.export_all")
+                    target:rule_add(rule)
+                    target:extraconf_set("rules", "utils.symbols.export_all", {export_classes = true})
+                end
             end
         end
-    end)
-end 
+    )
+end
 
 target("shared_lib")
 do
-    set_kind("shared")    
+    set_kind("shared")
     add_files("src/shared_lib/libabc_shared.cpp")
 end
 
@@ -30,73 +33,3 @@ do
     add_files("src/main/main.cpp")
     add_deps("shared_lib")
 end
-
---
--- If you want to known more usage about xmake, please see https://xmake.io
---
--- ## FAQ
---
--- You can enter the project directory firstly before building project.
---
---   $ cd projectdir
---
--- 1. How to build project?
---
---   $ xmake
---
--- 2. How to configure project?
---
---   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
---
--- 3. Where is the build output directory?
---
---   The default output directory is `./build` and you can configure the output directory.
---
---   $ xmake f -o outputdir
---   $ xmake
---
--- 4. How to run and debug target after building project?
---
---   $ xmake run [targetname]
---   $ xmake run -d [targetname]
---
--- 5. How to install target to the system directory or other output directory?
---
---   $ xmake install
---   $ xmake install -o installdir
---
--- 6. Add some frequently-used compilation flags in xmake.lua
---
--- @code
---    -- add debug and release modes
---    add_rules("mode.debug", "mode.release")
---
---    -- add macro definition
---    add_defines("NDEBUG", "_GNU_SOURCE=1")
---
---    -- set warning all as error
---    set_warnings("all", "error")
---
---    -- set language: c99, c++11
---    set_languages("c99", "c++11")
---
---    -- set optimization: none, faster, fastest, smallest
---    set_optimize("fastest")
---
---    -- add include search directories
---    add_includedirs("/usr/include", "/usr/local/include")
---
---    -- add link libraries and search directories
---    add_links("tbox")
---    add_linkdirs("/usr/local/lib", "/usr/lib")
---
---    -- add system link libraries
---    add_syslinks("z", "pthread")
---
---    -- add compilation and link flags
---    add_cxflags("-stdnolib", "-fno-strict-aliasing")
---    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
---
--- @endcode
---
-
